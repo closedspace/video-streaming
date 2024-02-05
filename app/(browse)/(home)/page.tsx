2e126a07d249ffa9a3c5fcdb65a90c1e254cbc91
Emil Momchilov <emil@pernik.org>
2024-02-05 23:51:40 +0200

    feat: add placeholder home page

---
+++ app/(browse)/(home)/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { Sidebar } from "../_components/sidebar/sidebar";
+
+export default function Home() {
+  return <div className="flex flex-col gap-y-4">Home page</div>;
+}
